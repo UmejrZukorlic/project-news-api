380af9fe6a003d8e6db7daa5713f4805601805cf
Umejr Zukorlic <umejrzuka@gmail.com>
2022-09-02 17:36:53 +0200

    doing hedline page

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,30 +1,34 @@
 import React from "react";
+import "./headline.css";
 import Layout from "../Layout";
 import API from "../../api/data.js";
 import { useState } from "react";
 import { useEffect } from "react";
 const Headline = (props) => {
   const [data, setData] = useState([]);
-  console.log(API());
+  // console.log(API());
   const fetchdata = API();
   useEffect(() => {
     if (fetchdata) {
       setData(fetchdata.articles);
       console.log(data);
     }
-  }, []);
+  }, [fetchdata]);
 
   return (
     <Layout>
-      {data.map((item) => {
-        return (
-          <div>
-            <h3>{item.source.name}</h3>
-            <p>{item.title}</p>
-            <img src={item.urlToImage} alt="" />
-          </div>
-        );
-      })}
+      <div className="headlineSection">
+        {data.map((item, index) => {
+          return (
+            <div key={index} className="newsCard">
+              <img src={item.urlToImage} alt="" />
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <p>- {item.source.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </Layout>
   );
 };
